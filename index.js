0c7440ff3239cfb9ef35753c5c9d3474202224e7
kfrn <knfrances@gmail.com>
2016-11-25 11:28:24 +1300

    Bundle files; correct start script; render basic text

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const React = require('react')
 const {createStore} = require('redux')
-const render = require('react-dom')
+const {render} = require('react-dom')
 
 const reducer = require('./reducer')
+const Guess = require('./views/Guess')
+
+const main = document.querySelector('main')
+const app = document.createElement('div')
+main.appendChild(app)
 
 const {subscribe, dispatch, getState} = createStore
 
 const initialState = {
-  location: {city: },
+  location: {city: 'London'},
   guesses: [
     {correctTemp: 12, guessedTemp: null, correct: false, attempted: false}
   ],
   score: 0
 }
+
+const store = createStore(reducer, initialState)
+
+const App = (props) =>
+    <div id='app'>
+        The city is London
+    </div>
+
+
+store.subscribe( () => {
+  const state = store.getState()
+  render(<App
+          state={state}
+          dispatch={dispatch}
+          />, app)
+})
+
+store.dispatch({type: 'INIT'})
