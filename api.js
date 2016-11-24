d4634133003c9b718588352ff555949b863238a9
eda003 <james.brown9809@hotmail.co.nz>
2016-11-25 10:36:03 +1300

    Add yahoo-weather npm; write API call

---
+++ api.js
@@ -0,0 +1,16 @@
+const weather = require('yahoo-weather')
+
+module.exports = getWeatherData
+
+function getWeatherData(city) {
+  weather(city).then(data => {
+    console.log("location is:", data.location.city + ", " + data.location.country)
+    console.log("lat and log are:", data.item.lat + ", " + data.item.long)
+    console.log("temperature is:", data.item.condition.temp)
+    console.log("conditions are:", data.item.condition.text)
+  }).catch(err => {
+    console.log(err);
+  })
+}
+
+getWeatherData('London')
