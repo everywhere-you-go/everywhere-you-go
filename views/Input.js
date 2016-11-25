cd1c502872bef165c74ca8f4005dce23450c9b31
kfrn <knfrances@gmail.com>
2016-11-25 14:05:46 +1300

    Using input text to make API call

--- views/Input.js
+++ views/Input.js
@@ -5,11 +5,13 @@ const getWeatherData = require('../api')
 const Input = (props) => {
   return <div>
             <p>Hello Input</p>
-            <input></input>
+            <input id='location'></input>
             <button type='submit' className='addquestion' onClick={() => {
-              getWeatherData('Wellington', (res) => {
-                console.log(res.item);
-                props.dispatch({type: 'ADD_LOCATION', payload: 'Wellington'})
+              var location = document.getElementById('location').value
+              getWeatherData(location, (res) => {
+                console.log("location is:", location);
+                console.log("temp is:", res.item.condition.temp);
+                props.dispatch({type: 'ADD_LOCATION', payload: location})
                 props.dispatch({type: 'ADD_QUESTION', payload: {correctTemp: res.item.condition.temp, guessedTemp: null, correct: false, attempted: false}})
               })
             }} >Add question</button>
