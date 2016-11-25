67ef05abd63253c6dc44d66e56665f662e76ec2b
kfrn <knfrances@gmail.com>
2016-11-25 16:10:19 +1300

    Remove console.logs

--- reducer.js
+++ reducer.js
@@ -12,7 +12,7 @@ module.exports = (state, action) => {
       newState.questions.push(payload)
       return newState
     case 'ATTEMPT_QUESTION':
-      console.log(newState)
+      // console.log(newState)
       // console.log("payload is", payload);
       newState.questions[payload.index].guessedTemp = payload.guess
       newState.questions[payload.index].correct = (newState.questions[payload.index].correctTemp == newState.questions[payload.index].guessedTemp)
@@ -20,10 +20,10 @@ module.exports = (state, action) => {
       return newState
     case 'INCREMENT_SCORE':
       newState.score += 1
-      console.log("score is", newState.score);
+      // console.log("score is", newState.score);
       return newState
     case 'CHECK_FINISHED':
-      console.log("Checking if finished...")
+      // console.log("Checking if finished...")
       let fiveQuestions = newState.questions.length === 5
       let allAttempted = true
       newState.questions.forEach((question) => {
