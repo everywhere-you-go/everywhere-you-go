a8258f2513b30731a44ca7388b0d47593eaba82d
kfrn <knfrances@gmail.com>
2016-11-25 14:41:44 +1300

    Amend templates & styling

--- views/Questions.js
+++ views/Questions.js
@@ -2,7 +2,7 @@ const React = require('react')
 
 const Questions = (props) => {
   return <div>
-          {props.questions.map((question) => <div>{question.city}</div>)}
+          {props.questions.map((question) => <div className='question'>What is the temperature in {question.city}?</div>)}
          </div>
 }
 
