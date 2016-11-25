ee1cbddd36ee7c6ea45d215bec3a58b2905e70a0
kfrn <knfrances@gmail.com>
2016-11-25 15:06:24 +1300

    Remove unneeded code

--- views/Questions.js
+++ views/Questions.js
@@ -15,14 +15,3 @@ const Questions = (props) => {
 }
 
 module.exports = Questions
-
-
-// <input id='location'></input>
-// <button type='submit' className='addquestion' onClick={() => {
-//   var location = document.getElementById('location').value
-//   // console.log("submitted location is", location);
-//   getWeatherData(location, (res) => {
-//     props.dispatch({type: 'ADD_LOCATION', payload: location})
-//     props.dispatch({type: 'ADD_QUESTION', payload: {city: location, correctTemp: res.item.condition.temp, guessedTemp: null, correct: false, attempted: false}})
-//   })
-// }} >Go!</button>
